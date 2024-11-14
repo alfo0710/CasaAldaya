73adf35e568da67f5bc7f6fdda14a3ee7349620c
ALFONSINA COIRO GIANNECHINI <56931132@ceibal.edu.uy>
2024-11-14 09:18:32 -0300

    Archivos de Perfil y Productos terminados!

--- java/perfil.js
+++ java/perfil.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const nombreSpan = document.getElementById("nombre");
+    const apellidoSpan = document.getElementById("apellido");
     const correoSpan = document.getElementById("correo");
-    const fechaNacimientoSpan = document.getElementById("fechaNacimiento");
     const generoSpan = document.getElementById("genero");
     const logoutBtn = document.getElementById("logoutBtn");
 
@@ -11,8 +11,8 @@ document.addEventListener("DOMContentLoaded", function() {
     if (isLoggedIn === "true" && storedUser) {
         // Rellena los datos del usuario en el perfil
         nombreSpan.textContent = storedUser.nombre;
+        apellidoSpan.textContent = storedUser.apellido;
         correoSpan.textContent = storedUser.email;
-        fechaNacimientoSpan.textContent = storedUser.fechaNacimiento;
         generoSpan.textContent = storedUser.genero;
     } else {
         // Si no está logueado, muestra el mensaje y redirige a la página de inicio de sesión
@@ -27,3 +27,17 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
+function cambiarImagen(event) {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(e) {
+        // Cambiar la imagen de perfil
+        const imagenPerfil = document.getElementById("imagenPerfil");
+        imagenPerfil.src = e.target.result;
+    }
+
+    if (file) {
+        reader.readAsDataURL(file);
+    }
+}
